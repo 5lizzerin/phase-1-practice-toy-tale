3f3f9d20721ad280b1f47656aa9a0142cce45726
Lizz <lizzcklein@gmail.com>
2023-09-18 10:44:37 -0400

    created branch, fetched toy data, set up cards and name

--- src/index.js
+++ src/index.js
@@ -13,3 +13,28 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+//create a card for each toy in json file
+//those cards will be added to a div in doc called "Toy Collection"
+
+
+//create a card
+//give each card info about toy that we get from json server
+//append to toy collection
+
+const toyUrl = "http://localhost:3000/toys";
+
+fetch(toyUrl)
+  .then(response => response.json())
+  .then(data => {
+    for (const toy of data){
+      const toyCard = document.createElement("div")
+      toyCard.className = "card"
+      const toyName = document.createElement("h2")
+      toyName.textContent = toy.name 
+    }
+    }
+
+
+
+  )
